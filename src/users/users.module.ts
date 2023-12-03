de8a17416041f35870b5085ec2f2126c88f9ce95
Ayush Singh <ayushmoca@gmail.com>
2023-12-03 21:12:03 +0530

    setup database connection

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { User } from './users.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])], // this stepcreates a repository for us
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}

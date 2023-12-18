8b8e3a1b7511e4f88bf9ee18699aa83fe6ab1af2
Ayush Singh <ayushmoca@gmail.com>
2023-12-18 08:04:10 +0530

    Generalizing the interceptor

--- src/interceptor/serialize.interceptor.ts
+++ src/interceptor/serialize.interceptor.ts
@@ -7,17 +7,23 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs';
 import { plainToClass } from 'class-transformer';
+import { UserDto } from 'src/users/dtos/user.dto'; // not needed anymore
 
 export class SerializeInterceptor implements NestInterceptor{
+  constructor(private dto: any) {}
+
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
      // Run something before a request is handled by the
      // request handler
-     console.log("Running before the handler", context);
+    //  console.log("Running before the handler", context);
 
      return handler.handle().pipe(
       map((data: any) => {
         // Run something before the response is sent out 
-        console.log("I'm running before the response is sent out", data);
+        // console.log("I'm running before the response is sent out", data);
+        return plainToClass(this.dto, data, {
+          excludeExtraneousValues: true, // ensures when we have the dto and turn it into JSON it is only going to share or expose different properties that are specifically marked with that "Exclude" keyword
+        });
       })
      );
   }
